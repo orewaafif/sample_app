cb2f110c3cc6a3d3e17b15591b217e0ffc1e005e
Afif Ashroff <afifashroff@gmail.com>
2018-04-24 00:37:25 +0800

    Create invalid form submission alert
    Invalid form submission alert which will render a partial and highlight the location of error in the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
   end
+
+  def create
+    @user = User.new(user_params) #user_params will specify the specific parameters that will be accepted. No more, no less
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+
+  private #keeping it private, not accessible publicly
+  #don't forget the right indentation to avoid accidental private methods
+    def user_params #the accepted parameters, including :user is always required
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 end
