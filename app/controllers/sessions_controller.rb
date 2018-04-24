1b7c5d313f19a684d225502c8fed12a38878c56c
Afif Ashroff <afifashroff@gmail.com>
2018-04-25 00:46:17 +0800

    Enable valid login with session
    Valid logins are now accepted. The header of the app will change according to login status with menu for user to choose including logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) #this uses the authenticate method that comes with has_secure_password
       #login user
+      log_in user
+      redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination' #not the final implementation
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  #Login the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  #Returns the current user info, if any
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  #Returns true if the user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+  
 end
