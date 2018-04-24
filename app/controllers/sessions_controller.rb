53e2202ac40e6f186e7edaf4942dda507062b0ca
Afif Ashroff <afifashroff@gmail.com>
2018-04-24 23:17:24 +0800

    Add user validation and invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password]) #this uses the authenticate method that comes with has_secure_password
+      #login user
+    else
+      flash[:danger] = 'Invalid email/password combination' #not the final implementation
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
