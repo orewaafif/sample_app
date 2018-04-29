9f754120262cb2f5c553a6be6ecdc9c6af8259ce
Afif Ashroff <afifashroff@gmail.com>
2018-04-29 15:22:20 +0800

    Add admin attribute for users
    Admin attribute is added to the users model. A test is created to ensure that the 'strong parameter' is there to disable modification on the admin status via web

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -45,4 +45,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                  user:   {
+                                    password: "",
+                                    password_confirmation: "",
+                                    admin: true
+                                  } }
+    assert_not @other_user.reload.admin?
+  end
+
 end
