2515f51517b29c791dbdca268f8bd10f279e58b3
Afif Ashroff <afifashroff@gmail.com>
2018-05-01 01:46:40 +0800

    Add function for activation digest
    A function is created in User.rb to create the activation digest. The activation digest will be used to verify that the user registered with a valid email address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
-  #before_save { self.email = email.downcase }
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true 
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_secure_password
 
@@ -42,5 +44,16 @@ class User < ApplicationRecord
     end
 
   end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end 
 
 end
